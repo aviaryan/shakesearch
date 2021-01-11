d1ae52f9a2290f23df0830e8ad7cc92cc0403c1f
Avi Aryan <avi.aryan123@gmail.com>
2021-01-11 16:04:06 +0530

    improve UI/UX

--- static/app.js
+++ static/app.js
@@ -11,13 +11,20 @@ const Controller = {
   },
 
   updateTable: (results) => {
-    const table = document.getElementById("table-body");
+    const table = document.getElementById("table");
+    // results can be null because of golang behavior
+    if (!results || results.length === 0) {
+      table.style.display = "none";
+      return;
+    }
+    table.style.display = "block";
+    const tableBody = document.getElementById("table-body");
     const rows = [];
     for (let result of results) {
       const row = `<tr><td>${result.match}</td><td>${result.work}</td></tr>`;
       rows.push(row);
     }
-    table.innerHTML = rows;
+    tableBody.innerHTML = rows.join('');
   },
 };
 
